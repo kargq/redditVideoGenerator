1809959c21bc243116fa39174b2b6f234fcd68d7
Kindeep <kk17xg@brocku.ca>
2019-05-24 18:51:01 -0400

    clips.py with methods to create clips for comments

--- AskReddit.py
+++ AskReddit.py
@@ -1,6 +1,21 @@
 import praw
 from clips import *
 
+FPS = 30
+
+
+def create_submission_video(submission, save_path):
+    clips = []
+    enm_imgs = 0
+    submission.sort = 'top'
+    for comment in submission.comments[:2]:
+        print("\nComment: ", comment.body)
+        clips.append(create_comment_clip(author=comment.author.name, content=comment.body))
+        enm_imgs = enm_imgs + 1
+    concat_clip = concatenate_videoclips(clips)
+    concat_clip.write_videofile(save_path, fps=FPS)
+
+
 print("Subscribe or i'll end humanity.")
 
 reddit = praw.Reddit(client_id='cr4TmbMtqyHJgQ',
@@ -11,20 +26,9 @@ a_subreddit = reddit.subreddit('AskReddit')
 
 print(a_subreddit.display_name, '\n' + ('=' * len(a_subreddit.display_name)))
 
-enm_imgs = 0
-
-clips = []
-
-for submission in a_subreddit.top(limit=1):
+for index, submission in enumerate(a_subreddit.top(limit=3)):
     print("\nTitle:", submission.title)
     print("Author:", submission.author)
     print("\n")
     submission.sort = 'top'
-    for comment in submission.comments[:2]:
-        print("\nComment: ", comment.body)
-        clips.append(create_comment_clip(author=comment.author.name, content=comment.body))
-        enm_imgs = enm_imgs + 1
-
-concat_clip = concatenate_videoclips(clips)
-
-concat_clip.write_videofile("media/test.mp4", fps=24)
+    create_submission_video(submission, "media/askreddit_submission_test" + str(index) + ".mp4")
