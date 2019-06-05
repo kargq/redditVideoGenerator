4fbf495080bd4ad5a1b597fe5b4af39e3c7d29b2
Kindeep <kk17xg@brocku.ca>
2019-06-05 16:36:19 -0400

    Video/Audio moslty functional

--- AskReddit.py
+++ AskReddit.py
@@ -2,16 +2,35 @@ import praw
 from clips import *
 
 FPS = 30
+DURATION: int = 60 * 10
+
+
+def random_title_msg():
+    return "Subscribe or I'll end humanity."
 
 
 def create_submission_video(submission, save_path):
     clips = []
     enm_imgs = 0
+    curr_duration: int = 0
+    clips.append(create_comment_clip(
+        submission.author.name, "AskReddit: " + submission.title))
+
+    clips.append(gen_title_message_clip(random_title_msg()))
+
     submission.sort = 'top'
-    for comment in submission.comments[:2]:
-        print("\nComment: ", comment.body)
-        clips.append(create_comment_clip(author=comment.author.name, content=comment.body))
+    submission.comments.replace_more(limit=50)
+    for comment in submission.comments:
+        print("\nComment: ", comment.body if comment.body else "[deleted]")
+        temp = create_comment_clip(
+            author=comment.author.name if comment.author else "[deleted]", content=comment.body if comment.body else "[deleted]")
+        clips.append(temp)
         enm_imgs = enm_imgs + 1
+        curr_duration = curr_duration + temp.duration
+        if curr_duration >= DURATION:
+            break
+        print(curr_duration)
+
     concat_clip = concatenate_videoclips(clips)
     concat_clip.write_videofile(save_path, fps=FPS)
 
@@ -31,4 +50,5 @@ for index, submission in enumerate(a_subreddit.top(limit=3)):
     print("Author:", submission.author)
     print("\n")
     submission.sort = 'top'
-    create_submission_video(submission, "media/askreddit_submission_test" + str(index) + ".mp4")
+    create_submission_video(
+        submission, "media/askreddit_submission_test" + str(index) + ".mp4")
--- clips.py
+++ clips.py
@@ -3,6 +3,12 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import numpy as np
 from moviepy.editor import *
+import soundfile as sf
+from pydub import AudioSegment
+from pydub.playback import play
+
+from gtts import gTTS
+from io import BytesIO
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -14,14 +20,36 @@ def gen_comment_image(author, content):
     d = ImageDraw.Draw(img)
     d.text((10, 10), author, fill=(79, 188, 255), font=fnt)
     d.text((10, 45), content, fill=(255, 255, 255), font=fnt)
-    # img.save(save_path)
     return np.array(img)
 
 
+def gen_title_message_image(msg):
+    img = Image.new('RGB', (WIDTH, HEIGHT), color=(26, 26, 27))
+    fnt = ImageFont.truetype('sans-serif.ttf', 60)
+    d = ImageDraw.Draw(img)
+    d.text((10, 10), msg, fill=(200, 188, 255), font=fnt)
+    return np.array(img)
+
+
+def gen_title_message_clip(msg):
+    background_clip = ImageClip(gen_title_message_image(msg))
+    audio_clip = gen_audio_clip(msg)
+    background_clip = background_clip.set_duration(audio_clip.duration)
+    background_clip = background_clip.set_audio(audio_clip)
+    return background_clip
+
+
 def create_comment_clip(author, content):
-    background_clip = ImageClip(gen_comment_image(author=author, content=content))
-    return background_clip.set_duration(10)
+    background_clip = ImageClip(
+        gen_comment_image(author=author, content=content))
+    audio_clip = gen_audio_clip(content)
+    background_clip = background_clip.set_duration(audio_clip.duration)
+    background_clip = background_clip.set_audio(audio_clip)
+    return background_clip
+
 
 def gen_audio_clip(text):
-
-    
+    tts = gTTS(text, 'en')
+    tts.save("temp.mp3")
+    clip = AudioFileClip("temp.mp3")
+    return clip
