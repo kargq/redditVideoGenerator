adb7db573e8dbd1281bdd50f770d72b295d9ca1d
Kindeep <kk17xg@brocku.ca>
2019-08-22 09:11:38 -0400

    All dependencies should now be in requirements.txt

--- AskReddit.py
+++ AskReddit.py
@@ -6,6 +6,7 @@ FPS = 30
 # DURATION: int = 25
 BACKGROUND_TRACK_VOLUME = 0.12
 DURATION: int = 60 * 10
+DESCRIPTION = "Yes I'm an actual robot. \n"
 
 
 def random_title_msg():
@@ -33,7 +34,8 @@ def create_submission_video(submission, save_path):
     for comment in submission.comments:
         print("\nComment: ", comment.body if comment.body else "[deleted]")
         temp = create_comment_clip(
-            author=comment.author.name if comment.author else "[deleted]", content=comment.body if comment.body else "[deleted]")
+            author=comment.author.name if comment.author else "[deleted]",
+            content=comment.body if comment.body else "[deleted]")
         clips.append(gen_transition_clip())
         clips.append(temp)
         enm_imgs = enm_imgs + 1
@@ -56,17 +58,45 @@ reddit = praw.Reddit(client_id='cr4TmbMtqyHJgQ',
                      client_secret='W63uiFhtJY3Lmy87lLlr0zZReRY',
                      user_agent='kindeep')
 
-a_subreddit = reddit.subreddit('AskReddit')
+type = input("1. Top 2. Custom 3. Hot: ")
 
-print(a_subreddit.display_name, '\n' + ('=' * len(a_subreddit.display_name)))
+if int(type) == 1:
+    a_subreddit = reddit.subreddit('AskReddit')
 
-for index, submission in enumerate(a_subreddit.top(limit=1)):
+    print(a_subreddit.display_name, '\n' + ('=' * len(a_subreddit.display_name)))
+
+    for index, submission in enumerate(a_subreddit.top(limit=1)):
+        print("\nTitle:", submission.title)
+        print("Author:", submission.author)
+        print("\n")
+        submission.sort = 'top'
+        path = "rtemp" + str(index) + ".mp4"
+        create_submission_video(submission, path)
+        upload_video(path, description="", title="AskReddit: " +
+                                                 submission.title, keywords="AskReddit, Reddit")
+        clean_temp()
+elif int(type) == 2:
+    url = input("Enter subreddit submission url: ")
+    submission = praw.models.Submission(reddit, url=url)
     print("\nTitle:", submission.title)
     print("Author:", submission.author)
     print("\n")
     submission.sort = 'top'
-    path = "rtemp" + str(index) + ".mp4"
+    path = "media/" + submission.title + "_" + submission.id + ".mp4"
     create_submission_video(submission, path)
-    upload_video(path, description="", title="AskReddit: " +
-                 submission.title, keywords="AskReddit, Reddit")
-    clean_temp()
+    upload_video(path,
+                 description=DESCRIPTION + "Link to subreddit post: " + url + "\n" + submission.title,
+                 title="AskReddit: " + submission.title, keywords="AskReddit, Reddit")
+elif int(type) == 3:
+    a_subreddit = reddit.subreddit('AskReddit')
+    print(a_subreddit.display_name, '\n' + ('=' * len(a_subreddit.display_name)))
+    for index, submission in enumerate(a_subreddit.hot(limit=1)):
+        print("\nTitle:", submission.title)
+        print("Author:", submission.author)
+        print("\n")
+        submission.sort = 'top'
+        path = "rtemp" + str(index) + ".mp4"
+        create_submission_video(submission, path)
+        upload_video(path, description="", title="AskReddit: " +
+                                                 submission.title, keywords="AskReddit, Reddit")
+        clean_temp()
